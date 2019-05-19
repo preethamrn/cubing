34c3a3f32133f122ac3b0eaba261f95732f4522b
preethamrn <preetham.in.1997@gmail.com>
2019-05-18 21:06:22 -0700

    Added basic structure for portfolio cards

--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -1,19 +1,22 @@
 <template>
-  <v-layout id='portfolio' class='anchor-content portfolio-content'>
+  <v-layout row wrap id='portfolio' class='anchor-content portfolio-content'>
     <v-flex offset-md3 offset-xs1 xs10>
       <span class='anchor-header'>PORTFOLIO</span>
       <div class='header-underline'></div>
     </v-flex>
     <v-flex offset-md3 offset-xs1 md6 xs10>
-      <span>portfolio1</span>
-      <span>portfolio2</span>
+      <PortfolioItem />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import PortfolioItem from '@/components/PortfolioItem'
 export default {
   name: 'PortfolioCard',
+  components: {
+    PortfolioItem
+  },
   data: () => ({
 
   })
@@ -27,7 +30,7 @@ export default {
 
 .anchor-header {
   font-size: 3em;
-  font-family: "Muli", Arial, Verdana, sans-serif;
+  font-family: 'Muli', Arial, Verdana, sans-serif;
   font-weight: 900;
   font-style: italic;
   color: rgba(255, 255, 255, 1)
@@ -35,7 +38,7 @@ export default {
 .anchor-content {
   font-size: 1.5em;
   font-weight: 650;
-  font-family: "Muli", Arial, Verdana, sans-serif;
+  font-family: 'Muli', Arial, Verdana, sans-serif;
   color: rgba(255, 255, 255, 1)
 }
 </style>
---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-flex md4 xs12>
+    <v-card>
+      <v-img
+        src='../projects/blobv1.png'
+        height='200px'
+      ></v-img>
+      <v-card-title primary-title>
+        <div>
+          <div class='headline'>Blob v0</div>
+          <span class='grey--text'>Technologies: </span>
+        </div>
+      </v-card-title>
+
+      <v-card-actions>
+        <v-btn flat>Link</v-btn>
+        <v-btn flat color='purple'><v-icon>fab fa-github</v-icon></v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon @click='show = !show'>
+          <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, 
+            not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! 
+            You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioItem',
+  data: () => ({
+    show: false
+  })
+}
+</script>
+
+<style>
+
+</style>
