afdefd744035d3b13dd88899846c9d70d2a2d33b
Preetham Narayanareddy <preethamrn@uber.com>
2019-07-16 11:24:43 -0700

    Added refs for the 'stickers'

--- src/views/3BLDtrainer.vue
+++ src/views/3BLDtrainer.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="3bldtrainer">
-    <h1>This is an about page</h1>
+    <h1>3BLD Trainer</h1>
+    <v-container>
+      <v-flex xs2 ref='s1'></v-flex>
+      <v-flex xs2 ref='s2'></v-flex>
+      <v-flex xs2 ref='s3'></v-flex>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'BLDTrainer3',
+  data: () => ({
+    colors: {
+      'U': 'white',
+      'L': 'orange',
+      'F': 'green',
+      'R': 'red',
+      'B': 'blue',
+      'D': 'yellow'
+    },
+    corners: ['UBL','URB','UFR','ULF','LUB','LFU','LDF','LBD','FUL','FRU','FDR','FLD','RUF','RBU','RDB','RFD','BUR','BLU','BDL','BRD','DFL','DRF','DBR','DLB'],
+    edges: ['UB','UR','UF','UL','LU','LF','LD','LB','FU','FR','FD','FL','RU','RB','RD','RF','BU','BL','BD','BR','DF','DR','DB','DL']
+  }),
+  methods: {
+    next () {
+      console.log(this.$refs['s1'])
+      console.log(this.$refs['s2'])
+      console.log(this.$refs['s3'])
+    }
+  },
+  mounted: function () {
+    this.next()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
