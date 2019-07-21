a3c1cc5e1c6285555bc0b31438e5d2409ada7bf9
preethamrn <preetham.in.1997@gmail.com>
2019-07-21 01:45:06 -0700

    Finished BLD trainer app

--- src/views/3BLDtrainer.vue
+++ src/views/3BLDtrainer.vue
@@ -2,13 +2,22 @@
   <div class="bldtrainer3">
     <v-container>
       <h1 style='padding: 20px 0 20px 0'>3BLD Trainer</h1>
+      <v-switch v-model='testCorners' label='Train Corners' color='success' @change='next'></v-switch>
       <v-layout row>
-        <div :style='{display: testCorners ? "inline" : "none", background: s1bg}' class='sticker-box'></div>
-        <div :style='{display: testCorners ? "inline" : "none", background: s2bg}' class='sticker-box'></div>
+        <div :style='{background: stickerBG[0]}' class='sticker-box'></div>
+        <div :style='{background: stickerBG[1]}' class='sticker-box'></div>
       </v-layout>
       <v-layout row>
-        <div :style='{display: testCorners ? "inline" : "none", background: s3bg}' class='sticker-box'></div>
-        <input class='' placeholder="click on mobile" />
+        <div :style='{display: testCorners ? "inline" : "none", background: stickerBG[2]}' class='sticker-box'></div>
+        <input class='mobile-input' placeholder="click me" />
+      </v-layout>
+      <v-layout v-if='correctAnswer !== ""' column>
+        <div v-if='correctAnswer === selectedAnswer'><h1 style='color: green'>Correct!</h1></div>
+        <div v-else>
+          <h1 style='color: red'>Incorrect</h1>
+          <h1>You chose: {{selectedAnswer}}, Correct Answer: {{correctAnswer}} </h1>
+        </div>
+        <div><h1>Time taken: {{formatTime(timeTaken)}}</h1></div>
       </v-layout>
     </v-container>
   </div>
@@ -26,21 +35,112 @@ export default {
       'B': 'blue',
       'D': 'yellow'
     },
-    corners: ['UBL','URB','UFR','ULF','LUB','LFU','LDF','LBD','FUL','FRU','FDR','FLD','RUF','RBU','RDB','RFD','BUR','BLU','BDL','BRD','DFL','DRF','DBR','DLB'],
-    edges: ['UB','UR','UF','UL','LU','LF','LD','LB','FU','FR','FD','FL','RU','RB','RD','RF','BU','BL','BD','BR','DF','DR','DB','DL'],
+    corners: {
+      'UBL': 'A',
+      'URB': 'B',
+      'UFR': 'C',
+      'ULF': 'D',
+      'LUB': 'E',
+      'LFU': 'F',
+      'LDF': 'G',
+      'LBD': 'H',
+      'FUL': 'I',
+      'FRU': 'J',
+      'FDR': 'K',
+      'FLD': 'L',
+      'RUF': 'M',
+      'RBU': 'N',
+      'RDB': 'O',
+      'RFD': 'P',
+      'BUR': 'Q',
+      'BLU': 'R',
+      'BDL': 'S',
+      'BRD': 'T',
+      'DFL': 'U',
+      'DRF': 'V',
+      'DBR': 'W',
+      'DLB': 'X'
+    },
+    edges: {
+      'UB': 'A',
+      'UR': 'B', 
+      'UF': 'C', 
+      'UL': 'D', 
+      'LU': 'E', 
+      'LF': 'F', 
+      'LD': 'G', 
+      'LB': 'H', 
+      'FU': 'I', 
+      'FR': 'J', 
+      'FD': 'K', 
+      'FL': 'L', 
+      'RU': 'M', 
+      'RB': 'N', 
+      'RD': 'O', 
+      'RF': 'P', 
+      'BU': 'Q', 
+      'BL': 'R', 
+      'BD': 'S', 
+      'BR': 'T', 
+      'DF': 'U', 
+      'DR': 'V', 
+      'DB': 'W', 
+      'DL': 'X'
+    },
     testCorners: true,
 
-    s1bg: 'red',
-    s2bg: 'blue',
-    s3bg: 'white'
+    selection: '',
+    stickerBG: ['red', 'blue', 'white'],
+
+    correctAnswer: '',
+    selectedAnswer: '',
+    realTimeStart: 0,
+    timeTaken: 0,
+
+    debouncer: false, // prevent multiple keypressed or keypressed during interval from checking
   }),
   methods: {
     next () {
+      this.debouncer = false
+      this.realTimeStart = window.performance.now()
 
+      if (this.testCorners) {
+        let keys = Object.keys(this.corners)
+        this.selection = keys[Math.floor(Math.random() * keys.length)]
+      } else {
+        let keys = Object.keys(this.edges)
+        this.selection = keys[Math.floor(Math.random() * keys.length)]
+      }
+      for (let i=0; i<this.selection.length; i++) {
+        this.stickerBG[i] = this.colors[this.selection[i]]
+      }
+      this.$forceUpdate()
+    },
+    checkKey (key) {
+      this.correctAnswer = (this.testCorners ? this.corners : this.edges)[this.selection].toUpperCase()
+      this.selectedAnswer = key.toUpperCase()
+      this.timeTaken = window.performance.now() - this.realTimeStart
+      return this.selectedAnswer === this.correctAnswer
+    },
+
+    eventHandler (event) {
+      if (this.debouncer || event.type !== 'keydown' || event.key.length > 1) return
+      this.debouncer = true
+      this.checkKey(event.key)
+      setTimeout(this.next, 1000)
+    },
+
+    formatTime (time) {
+      let date = new Date(time)
+      if (time < 60000) return date.getSeconds() + '.' + date.getMilliseconds() + 's'
+      else if (time < 3600000) return date.getMinutes() + ':' + date.getSeconds() + '.' + date.getMilliseconds()
+      else if (time < 86400000) return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '.' + date.getMilliseconds()
+      else return 'dude. that way seriously way too long I lost track of time'
     }
   },
   mounted: function () {
     this.next()
+    document.addEventListener('keydown', this.eventHandler)
   }
 }
 </script>
@@ -50,10 +150,19 @@ export default {
   height: 100px !important;
   width: 100px !important;
   margin: 5px;
-  background: red;
+  background: white;
+  border: thick solid black;
 }
 .bldtrainer3 {
   background: #eeeeee;
   height: 100vh;
+}
+.mobile-input {
+  display: none;
+}
+@media only screen and (max-width: 1000px) {
+  .mobile-input {
+    display: inline;
+  }
 }
 </style>
