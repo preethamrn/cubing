51a5e788fd4a5c81522043ee36b224cccb83abb8
preethamrn <preetham.in.1997@gmail.com>
2020-08-21 01:21:01 -0700

    [stanz] Simplified team selection screen

--- src/components/StanzSheet/AgentSelect.vue
+++ src/components/StanzSheet/AgentSelect.vue
@@ -1,22 +1,10 @@
 <template>
   <div>
+    <v-text-field style='width: 0px; position: fixed; bottom: 0' v-model='search' @keydown.enter='$emit("input", agentsList[0].name)'></v-text-field>
     <v-item-group :value='value' @change='$emit("input", $event)'>
       <v-row no-gutters>
         <v-item 
-          v-for='agentData in [
-            {name: "Breach", img: "valorant/breach.png"},
-            {name: "Brimstone", img: "valorant/brimstone.png"},
-            {name: "Cypher", img: "valorant/cypher.png"},
-            {name: "Jett", img: "valorant/jett.png"},
-            {name: "Killjoy", img: "valorant/killjoy.png"},
-            {name: "Omen", img: "valorant/omen.png"},
-            {name: "Phoenix", img: "valorant/phoenix.png"},
-            {name: "Raze", img: "valorant/raze.png"},
-            {name: "Reyna", img: "valorant/reyna.png"},
-            {name: "Sage", img: "valorant/sage.png"},
-            {name: "Sova", img: "valorant/sova.png"},
-            {name: "Viper", img: "valorant/viper.png"},
-          ]'
+          v-for='agentData in agentsList'
           :key='agentData.name'
           v-slot:default='{ active, toggle }' :value='agentData.name'>
             <v-col cols='1'>
@@ -43,10 +31,30 @@
 export default {
   name: 'agent-select',
   data () {
-    return {}
+    return {
+      search: '',
+    }
   },
   props: {
     value: String,
+  },
+  computed: {
+    agentsList () {
+      return [
+        {name: "Breach", img: "valorant/breach.png"},
+        {name: "Brimstone", img: "valorant/brimstone.png"},
+        {name: "Cypher", img: "valorant/cypher.png"},
+        {name: "Jett", img: "valorant/jett.png"},
+        {name: "Killjoy", img: "valorant/killjoy.png"},
+        {name: "Omen", img: "valorant/omen.png"},
+        {name: "Phoenix", img: "valorant/phoenix.png"},
+        {name: "Raze", img: "valorant/raze.png"},
+        {name: "Reyna", img: "valorant/reyna.png"},
+        {name: "Sage", img: "valorant/sage.png"},
+        {name: "Sova", img: "valorant/sova.png"},
+        {name: "Viper", img: "valorant/viper.png"},
+      ].filter((v) => (!this.search || v.name.toLowerCase().includes(this.search)))
+    }
   }
 }
 </script>
--- src/components/StanzSheet/PlayerSelect.vue
+++ src/components/StanzSheet/PlayerSelect.vue
@@ -1,66 +1,79 @@
 <template>
   <div class='player-select'>
-    <v-stepper v-model='step' vertical>
-      <v-stepper-step step='1'>Choose a map</v-stepper-step>
-      <v-item-group v-model='map'>
-        <v-container class='pa-0'>
-          <v-row justify='start'>
-            <v-col
-              v-for='mapData in [{name: "Ascent", img: "https://liquipedia.net/commons/images/9/94/Ascent_Map_Banner.png"}, {name: "Bind", img: "https://liquipedia.net/commons/images/b/bf/Bind_Map_Banner.png"}, {name: "Haven", img: "https://liquipedia.net/commons/images/2/28/Haven_Map_Banner.png"}, {name: "Split", img: "https://liquipedia.net/commons/images/4/4c/Split_Map_Banner.png"}]'
-              :key='mapData.name'
-              cols='12'
-              md='6'
-            >
-              <v-item v-slot:default='{ active, toggle }' :value='mapData.name'>
-                <v-img
-                  :src='mapData.img'
-                  :aspect-ratio="16/9"
-                  height='150'
-                  class='pa-2'
-                  @click='toggle(); step=2'
-                >
-                  <v-row align='end' class='fill-height white--text'>
-                    <h1 :class='{"active": active}'>{{mapData.name}}</h1>
-                  </v-row>
-                </v-img>
-              </v-item>
+    <v-container fluid>
+    <v-row>
+      <v-col cols='5'>
+        <h2>Map</h2>
+        <v-item-group v-model='map'>
+          <v-container class='pa-2'>
+            <v-row justify='start'>
+              <v-col
+                v-for='mapData in [{name: "Ascent", img: "https://liquipedia.net/commons/images/9/94/Ascent_Map_Banner.png"}, {name: "Bind", img: "https://liquipedia.net/commons/images/b/bf/Bind_Map_Banner.png"}, {name: "Haven", img: "https://liquipedia.net/commons/images/2/28/Haven_Map_Banner.png"}, {name: "Split", img: "https://liquipedia.net/commons/images/4/4c/Split_Map_Banner.png"}]'
+                :key='mapData.name'
+                cols='12'
+                md='6'
+              >
+                <v-item v-slot:default='{ active, toggle }' :value='mapData.name'>
+                  <v-img
+                    :src='mapData.img'
+                    :aspect-ratio="16/9"
+                    height='150'
+                    class='pa-2'
+                    @click='toggle'
+                  >
+                    <v-row align='end' class='fill-height white--text'>
+                      <h1 :class='{"active": active}'>{{mapData.name}}</h1>
+                    </v-row>
+                  </v-img>
+                </v-item>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-item-group>
+
+        <h2>Teams</h2>
+        <v-container>
+          <v-text-field v-model='teams[0].team' label='Team 1' outlined></v-text-field>
+          <v-text-field v-model='teams[1].team' label='Team 2' outlined></v-text-field>
+        </v-container>
+
+        <h2>VOD URL</h2>
+        <v-container>
+          <v-text-field v-model='vodURL' label='URL (optional)' outlined></v-text-field>
+        </v-container>
+      </v-col>
+      <v-col cols='7'>
+        <h2>Player Selection</h2>
+        <v-container fluid>
+          <v-row>
+            <v-col v-for='j in [0, 1]' :key='j' cols='6'>
+              <h3>{{teams[j].team}}</h3>
+              <v-card>
+                <v-stepper v-model='currentlySelectingPlayer[j]' vertical>
+                  <template v-for='i in [0, 1, 2, 3, 4]'>
+                    <v-stepper-step :key='`${j}${i}-step`' :step='i+1' :complete='!!teams[j].players[i].agent' editable>
+                      Player {{i+1}}{{teams[j].players[i].name ? `: ${teams[j].players[i].name}` : ''}} {{teams[j].players[i].agent ? `- ${teams[j].players[i].agent}` : ''}}
+                    </v-stepper-step>
+                    <v-stepper-content :key='`${j}${i}-content`' :step='i+1'>
+                      <v-text-field v-model='teams[j].players[i].name' :placeholder='`Player ${i + 1}`' :ref='`playerInput-${i},${j}`' outlined></v-text-field>
+                      <agent-select :value='teams[j].players[i].agent' @input='($event) => { 
+                        teams[j].players[i].agent = $event
+                        if ($event) {
+                          currentlySelectingPlayer[j] = i+2
+                          if (i < 4) $refs[`playerInput-${i+1},${j}`][0].focus()
+                          $forceUpdate()
+                        }
+                      }' />
+                    </v-stepper-content>
+                  </template>
+                </v-stepper>
+              </v-card>
             </v-col>
           </v-row>
         </v-container>
-      </v-item-group>
-
-      <v-stepper-step step='2'>Choose the teams</v-stepper-step>
-      <v-container>
-        <v-text-field v-model='teams[0].team' label='Team 1' outlined></v-text-field>
-        <v-text-field v-model='teams[1].team' label='Team 2' outlined></v-text-field>
-      </v-container>
-
-      <v-stepper-step step='3'>Player Selection</v-stepper-step>
-      <v-container fluid>
-        <v-row>
-          <v-col v-for='j in [0, 1]' :key='j' cols='6'>
-            <v-card>
-              <v-stepper v-model='currentlySelectingPlayer[j]' vertical>
-                <template v-for='i in [0, 1, 2, 3, 4]'>
-                  <v-stepper-step :key='`${j}${i}-step`' :step='i+1' :complete='!!teams[j].players[i].agent' editable>
-                    Player {{i+1}}{{teams[j].players[i].name ? `: ${teams[j].players[i].name}` : ''}} {{teams[j].players[i].agent ? `- ${teams[j].players[i].agent}` : ''}}
-                  </v-stepper-step>
-                  <v-stepper-content :key='`${j}${i}-content`' :step='i+1'>
-                    <v-text-field v-model='teams[j].players[i].name' :placeholder='`Player ${i + 1}`' outlined></v-text-field>
-                    <agent-select :value='teams[j].players[i].agent' @input='($event) => { teams[j].players[i].agent = $event; if ($event) {currentlySelectingPlayer[j] = i+2; $forceUpdate();} }' />
-                  </v-stepper-content>
-                </template>
-              </v-stepper>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <v-stepper-step step='4'>VOD URL (optional)</v-stepper-step>
-      <v-container>
-        <v-text-field v-model='vodURL' label='URL' outlined></v-text-field>
-      </v-container>
-    </v-stepper>
+      </v-col>
+    </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -73,7 +86,6 @@ export default {
   },
   data () {
     return {
-      step: 1,
       map: 'Ascent',
       vodURL: '',
       teams: [{team: '', players: [{}, {}, {}, {}, {}]}, {team: '', players: [{}, {}, {}, {}, {}]}], // list of player objects {name, agent}
@@ -84,9 +96,6 @@ export default {
     value: Object,
   },
   watch: {
-    step2 () {
-      if (this.teams[0].team !== '' && this.teams[1].team !== '') this.step = 3
-    },
     teamSelection: {
       deep: true,
       handler () {
@@ -95,9 +104,6 @@ export default {
     }
   },
   computed: {
-    step2 () {
-      return this.teams[0].team + this.teams[1].team
-    },
     teamSelection () {
       let vodID = ''
       let vodType = ''
