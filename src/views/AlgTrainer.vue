b4ec0c1a9c227e0d1fd2d6558a04567f34a402eb
preethamrn <preetham.in.1997@gmail.com>
2020-10-27 22:38:21 -0700

    Add check for cube is solved

--- src/views/AlgTrainer.vue
+++ src/views/AlgTrainer.vue
@@ -7,11 +7,12 @@
 </template>
 
 <script>
-import { Sequence } from "cubing/alg"
+import { Sequence, BareBlockMove } from "cubing/alg"
 import {
   connect,
   debugKeyboardConnect,
 } from "cubing/bluetooth"
+import { KPuzzle, Puzzles, EquivalentTransformations } from "cubing/kpuzzle"
 import { TwistyPlayer } from "cubing/twisty"
 import Scramble from "../components/AlgTrainer/Scramble"
 
@@ -34,6 +35,7 @@ export default {
     twistyPlayer: null, // TODO: display just the cube and not the entire player window
     scramble: "R U2 R' U2 R' U' R U R U' R' U2 R' U2 R",
     moves: [],
+    puzzleState: null,
   }),
   methods: {
     async connect () {
@@ -42,6 +44,7 @@ export default {
       window.puzzle.addMoveListener((e) => {
         this.twistyPlayer.experimentalAddMove(e.latestMove)
         this.moves.push(e.latestMove)
+        this.puzzleState.applyBlockMove(e.latestMove)
         // TODO: use cubing/kpuzzle or cube.js to figure out if the cube is in a solved state after applying all the moves.
         console.log(e.latestMove)
       })
@@ -53,8 +56,14 @@ export default {
   mounted () {
     document.addEventListener('keydown', (e) => {
       console.log(e)
-      if ('urfdlbURFDLB'.split('').includes(e.key)) this.moves.push({family: e.key.toUpperCase(), amount: 1})
+      if ('urfdlbURFDLB'.split('').includes(e.key)) {
+        let m = BareBlockMove(e.key.toUpperCase(), 1)
+        this.moves.push(m)
+        this.puzzleState.applyBlockMove(m)
+        console.log("Cube is solved: ", EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, new KPuzzle(Puzzles['3x3x3']).state))
+      }
     })
+    this.puzzleState = new KPuzzle(Puzzles['3x3x3'])
     this.twistyPlayer = new TwistyPlayer({ alg: new Sequence([]) })
     document.querySelector('#twisty').appendChild(this.twistyPlayer)
     
