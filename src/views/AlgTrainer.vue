d7cf344f1e8dfd5f544240d976648b3aa319fa8f
preethamrn <preetham.in.1997@gmail.com>
2020-10-28 21:26:57 -0700

    Implement timer for algtrainer

--- src/views/AlgTrainer.vue
+++ src/views/AlgTrainer.vue
@@ -3,11 +3,12 @@
     <button @click='connect'>Connect</button>
     <scramble :scramble='scramble' :moves='moves' />
     <div id='twisty'></div>
+    <div>{{elapsedTime}}</div>
   </div>
 </template>
 
 <script>
-import { Sequence, BareBlockMove } from "cubing/alg"
+import { invert, parse, Sequence, BareBlockMove } from "cubing/alg"
 import {
   connect,
   debugKeyboardConnect,
@@ -15,6 +16,7 @@ import {
 import { KPuzzle, Puzzles, EquivalentTransformations } from "cubing/kpuzzle"
 import { TwistyPlayer } from "cubing/twisty"
 import Scramble from "../components/AlgTrainer/Scramble"
+import { clearInterval, setInterval } from 'timers';
 
 async function asyncSetup(twistyPlayer) {
   console.log("asyncSetup")
@@ -36,32 +38,52 @@ export default {
     scramble: "R U2 R' U2 R' U' R U R U' R' U2 R' U2 R",
     moves: [],
     puzzleState: null,
+
+    timerID: null,
+    startTime: null,
+    elapsedTime: null,
   }),
   methods: {
     async connect () {
       const acceptAllDevices = false
       window.puzzle = await connect({ acceptAllDevices })
       window.puzzle.addMoveListener((e) => {
+        if (this.startTime === null) {
+          this.puzzleState.applyAlg(invert(parse(this.scramble)))
+          this.startTimer()
+        }
         this.twistyPlayer.experimentalAddMove(e.latestMove)
         this.moves.push(e.latestMove)
         this.puzzleState.applyBlockMove(e.latestMove)
-        // TODO: use cubing/kpuzzle or cube.js to figure out if the cube is in a solved state after applying all the moves.
-        console.log(e.latestMove)
+        // TODO: ensure that this function isn't too expensive so timing doesn't have latency.
+        if (EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, new KPuzzle(Puzzles['3x3x3']).state)) {
+          this.stopTimer()
+          this.moves = []
+        }
       })
       // window.puzzle.addOrientationListener(() => {
       //   // TODO
       // })
+    },
+    startTimer () {
+      this.startTime = performance.now()
+      this.timerID = setInterval(() => {
+        this.elapsedTime = performance.now() - this.startTime
+      }, 10)
+    },
+    stopTimer () {
+      clearInterval(this.timerID)
+      this.startTime = null
+      this.timerID = null
     }
   },
   mounted () {
+    // TODO: remove this testing code.
     document.addEventListener('keydown', (e) => {
-      console.log(e)
       if ('urfdlbURFDLB'.split('').includes(e.key)) {
         let m = BareBlockMove(e.key.toUpperCase(), 1)
         this.moves.push(m)
         this.puzzleState.applyBlockMove(m)
-        // TODO: ensure that this function isn't too expensive so timing doesn't have latency.
-        console.log("Cube is solved: ", EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, new KPuzzle(Puzzles['3x3x3']).state))
       }
     })
     this.puzzleState = new KPuzzle(Puzzles['3x3x3'])
