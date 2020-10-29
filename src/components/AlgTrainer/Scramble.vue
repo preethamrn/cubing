abde93e104f20d86154a0880f61ee2bbba946fe8
preethamrn <preetham.in.1997@gmail.com>
2020-10-28 21:11:13 -0700

    Implemented scramble display

--- src/components/AlgTrainer/Scramble.vue
+++ src/components/AlgTrainer/Scramble.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="scramble">
-    <div> {{processedMoves}} </div>
-    <div> {{scramble}} </div>
-    <div> {{difference}} </div>
+    <span class="correct"> {{algToString(difference.correct)}} </span>
+    <span class="incorrect"> {{algToString(difference.incorrect)}} </span>
+    <span class="partial"> {{algToString(difference.partial)}} </span>
+    <span class="todo"> {{algToString(difference.todo)}} </span>
   </div>
 </template>
 
 <script>
-import { algToString, coalesceBaseMoves, parse, BareBlockMove, Sequence } from "cubing/alg"
+import { algToString, coalesceBaseMoves, invert, parse, BareBlockMove, Sequence } from "cubing/alg"
 
 export default {
   name: "scramble",
@@ -18,13 +19,24 @@ export default {
     moves: Array,
     scramble: String,
   },
+  methods: {
+    algToString (moves) {
+      return algToString(new Sequence(moves))
+    }
+  },
   watch: {
     moves () {
       let processed = this.moves.slice()
       let oldStr = algToString(new Sequence(processed))
       while (true) { // eslint-disable-line
         processed = coalesceBaseMoves(new Sequence(processed)).nestedUnits
-        processed = processed.map(v => (new BareBlockMove(v.family, v.amount % 4))).filter(v => v.amount)
+        processed = processed.map(v => {
+          let newAmount = v.amount % 4
+          if (newAmount === -2) newAmount = 2
+          if (newAmount === -3) newAmount = 1
+          if (newAmount === 3) newAmount = -1
+          return new BareBlockMove(v.family, newAmount)
+        }).filter(v => v.amount)
         let newStr = algToString(new Sequence(processed))
         if (newStr === oldStr) break
         else oldStr = newStr
@@ -56,8 +68,7 @@ export default {
       }
       let correct = this.processedMoves.slice(0, j)
       let todo = this.processedScramble.slice(i, this.processedScramble.length)
-      let incorrect = this.processedMoves.slice(i, this.processedMoves.length)
-      incorrect.reverse()
+      let incorrect = invert(new Sequence(this.processedMoves.slice(i, this.processedMoves.length))).nestedUnits
       return {
         correct,
         incorrect,
@@ -70,5 +81,15 @@ export default {
 </script>
 
 <style scoped>
-
+.correct {
+  background-color: rgba(0, 255, 0, 1);
+}
+.incorrect {
+  background-color: rgba(255, 0, 0, 1);
+}
+.partial {
+  background-color: rgba(255, 196, 0, 1);
+}
+.todo {
+}
 </style>
