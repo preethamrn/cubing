7e1a749c2d15fb58ae71172af833141213dd600b
preethamrn <preetham.in.1997@gmail.com>
2020-10-31 04:14:40 -0700

    Almost fixed wide moves/rotations case for scramble differences
    Only exception is leading rotation moves

--- src/components/AlgTrainer/Scramble.vue
+++ src/components/AlgTrainer/Scramble.vue
@@ -15,12 +15,13 @@ import { KPuzzle, Puzzles, EquivalentTransformations } from "cubing/kpuzzle"
 function mergeMappings (currentMapping, newMapping) {
   let merged = {}
   Object.keys(currentMapping).forEach((k) => {
-    if (newMapping[k]) merged[k] = currentMapping[newMapping[k]]
+    if (newMapping[currentMapping[k]]) merged[k] = newMapping[currentMapping[k]]
     else merged[k] = currentMapping[k]
   })
   return merged
 }
 
+// eslint-disable-next-line
 function invertMapping (mapping) {
   let inverse = {}
   Object.keys(mapping).forEach((k) => {
@@ -32,7 +33,8 @@ function invertMapping (mapping) {
 function fullCoalesceBaseMoves (moves) {
   let processed = moves.slice()
   let oldStr = algToString(new Sequence(processed))
-  while (true) { // eslint-disable-line
+  // eslint-disable-next-line
+  while (true) {
     processed = coalesceBaseMoves(new Sequence(processed)).nestedUnits
     processed = processed.map(v => {
       let newAmount = v.amount % 4
@@ -51,7 +53,7 @@ function fullCoalesceBaseMoves (moves) {
 export default {
   name: "scramble",
   data: () => ({
-    moveQueue: [], // use a move queue so that we can turn faster than we process moves
+    latestMove: 0, // we need to process multiple moves at a time since we can turn faster than we process moves
 
     // these indices store the values up to (and not including) which have been processed
     puzzleState: null,
@@ -63,8 +65,7 @@ export default {
     difference: {},
   }),
   props: {
-    move: Object,
-    id: Number,
+    moves: Array,
     scramble: String,
   },
   methods: {
@@ -95,52 +96,57 @@ export default {
     // Start off with puzzleState and scrambleState equivalent to the parent (ie, AlgTrainer) cube state => apply the inverse scramble.
     // Use EquivalentTransformation for each cube move to see which step in the scramble it is equivalent to (if you skip steps, this could cause an issue). The scramble cube state will need to be rotated with the appropriate rotations (tracked by orientation state).
     // Incorrect moves are any extra moves that don't match up. However, if we end up getting a state match, we reset the incorrect moves list.
-    // Partial moves are moves where the current inverse mapped move matches with the current scramble move.
-    // movesToExec are the inverseMapped cube moves along with any rotations performed at the step (assuming a new step is just completed). Any time we update the puzzleState, we MUST also update movesToExec.
+    // Partial moves are moves where the current mapped move matches with the current scramble move.
+    // movesToExec are the mapped cube moves along with any rotations performed at the step (assuming a new step is just completed). Any time we update the puzzleState, we MUST also update movesToExec.
     // Limitations:
     // 1. If user performs any incorrect wide moves/rotations this won't track that and instead will ask the user to perform the inverse single layer turn.
     // 2. Skipping steps or going to a previous step won't work (because we only check EquivalentTransformation for the current step in the scramble).
-    findDifference (move) {
+    findDifference () {
       let j = this.scrambleIndex, todoJ = null
       let partial = [], correct = [], incorrect = [], todo = []
       let movesToExec = []
 
-      let actualMove = new BareBlockMove(this.processedScramble[j].inverseMapping[move.family], move.amount)
-      console.log('processing: ', actualMove)
-      this.puzzleState.applyBlockMove(actualMove)
-      movesToExec.push(actualMove)
-      while (j < this.processedScramble.length) {
-        let alg = {}
-        if (!this.processedScramble[j].rotation) {
-          alg = new Sequence([this.processedScramble[j].move])
-        } else {
-          alg = new Sequence([this.processedScramble[j].move, new BareBlockMove(this.processedScramble[j].rotation.family, -this.processedScramble[j].rotation.amount)])
-        }
-        this.scrambleState.applyAlg(alg)
-        let equivalent = EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, this.scrambleState.state)
-        if (equivalent) {
-          if (this.processedScramble[j].rotation) {
-            this.scrambleState.applyBlockMove(this.processedScramble[j].rotation)
-            this.puzzleState.applyBlockMove(this.processedScramble[j].rotation)
-            movesToExec.push(this.processedScramble[j].rotation)
+      while (this.latestMove < this.moves.length) {
+        let move = this.moves[this.latestMove]
+        let actualMove = new BareBlockMove(this.processedScramble[j].mapping[move.family], move.amount)
+        console.log('processing: ', actualMove)
+        this.puzzleState.applyBlockMove(actualMove)
+        movesToExec.push(actualMove)
+        while (j < this.processedScramble.length) {
+          let alg = {}
+          // TODO: instead of multiple cases for rotations vs no rotation, just have all the adjacent rotations in a single "move". This fixes the error when a correct move is marked incorrect (eg. y R, B => incorrect: B', todo: y R)
+          if (!this.processedScramble[j].rotation) {
+            alg = new Sequence([this.processedScramble[j].move])
+          } else {
+            alg = new Sequence([this.processedScramble[j].move, new BareBlockMove(this.processedScramble[j].rotation.family, -this.processedScramble[j].rotation.amount)])
           }
-          this.incorrectMoves = []
-          actualMove = null // the move has been consumed and we should stop processing it
-          j++
-        } else {
-          this.scrambleState.applyAlg(invert(alg))
-          // we need to double check if actualMove hasn't already been consumed due to cases like (scramble: U2 y R B, moves: U2 R. the R would show up as an incomplete since we didn't break out of the loop after the equivalence)
-          if (actualMove) {
-            if (this.incorrectMoves.length == 0 && actualMove.family === this.processedScramble[j].move.family) {
-              // TODO: fix partial for case where there is an incorrect move but the next move is partially correct (eg. scramble: U2 R, moves: U F. We should show incorrect: F' + partial: U2)
-              partial = [this.processedScramble[j].move]
-              todoJ = j+1
-            } else {
-              this.incorrectMoves = fullCoalesceBaseMoves(this.incorrectMoves.concat(actualMove))
+          this.scrambleState.applyAlg(alg)
+          let equivalent = EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, this.scrambleState.state)
+          if (equivalent) {
+            if (this.processedScramble[j].rotation) {
+              this.scrambleState.applyBlockMove(this.processedScramble[j].rotation)
+              this.puzzleState.applyBlockMove(this.processedScramble[j].rotation)
+              movesToExec.push(this.processedScramble[j].rotation)
             }
+            this.incorrectMoves = []
+            actualMove = null // the move has been consumed and we should stop processing it
+            j++
+          } else {
+            this.scrambleState.applyAlg(invert(alg))
+            // we need to double check if actualMove hasn't already been consumed due to cases like (scramble: U2 y R B, moves: U2 R. the R would show up as an incomplete since we didn't break out of the loop after the equivalence)
+            if (actualMove) {
+              if (this.incorrectMoves.length == 0 && actualMove.family === this.processedScramble[j].move.family) {
+                // TODO: fix partial for case where there is an incorrect move but the next move is partially correct (eg. scramble: U2 R, moves: U F. We should show incorrect: F' + partial: U2)
+                partial = [this.processedScramble[j].move]
+                todoJ = j+1
+              } else {
+                this.incorrectMoves = fullCoalesceBaseMoves(this.incorrectMoves.concat(actualMove))
+              }
+            }
+            break
           }
-          break
         }
+        this.latestMove++
       }
       correct = this.processedScramble.map(v => v.move).slice(0, j)
       todo = this.processedScramble.map(v => v.move).slice(todoJ || j, this.processedScramble.length)
@@ -158,33 +164,28 @@ export default {
     },
   },
   watch: {
-    move () {
-      if (this.move) this.moveQueue.push(this.move.move)
-    },
-    moveQueue () {
-      if (this.moveQueue.length > 0) {
-        this.findDifference(this.moveQueue[0])
-        this.moveQueue = this.moveQueue.slice(1, this.moveQueue.length) // TODO: ensure no race conditions with move() watcher
-      }
+    moves () {
+      this.findDifference()
     },
     processedScramble () {
       this.reset()
     },
   },
   computed: {
+    // TODO: Update processed so that each "move" actually contains a single layer turn as well as all the preceeding rotations (and final move also contains any following rotations)
     processedScramble () {
       let parsed = parse(this.scramble).nestedUnits.slice()
       let processed = []
       let mapping = {R: 'R', L: 'L', F: 'F', B: 'B', U: 'U', D: 'D'}
       let mapRotation = (v) => {
-        if (v.family === 'y' && v.amount === -1) mapping = mergeMappings(mapping, {R: 'B', L: 'F', F: 'R', B: 'L'})
-        else if (v.family === 'y' && v.amount === 1) mapping = mergeMappings(mapping, {R: 'F', L: 'B', F: 'L', B: 'R'})
+        if (v.family === 'y' && v.amount === 1) mapping = mergeMappings(mapping, {R: 'B', L: 'F', F: 'R', B: 'L'})
+        else if (v.family === 'y' && v.amount === -1) mapping = mergeMappings(mapping, {R: 'F', L: 'B', F: 'L', B: 'R'})
         else if (v.family === 'y' && v.amount === 2) mapping = mergeMappings(mapping, {R: 'L', L: 'R', F: 'B', B: 'F'})
-        else if (v.family === 'x' && v.amount === -1) mapping = mergeMappings(mapping, {U: 'F', D: 'B', F: 'D', B: 'U'})
-        else if (v.family === 'x' && v.amount === 1) mapping = mergeMappings(mapping, {U: 'B', D: 'F', F: 'U', B: 'D'})
+        else if (v.family === 'x' && v.amount === 1) mapping = mergeMappings(mapping, {U: 'F', D: 'B', F: 'D', B: 'U'})
+        else if (v.family === 'x' && v.amount === -1) mapping = mergeMappings(mapping, {U: 'B', D: 'F', F: 'U', B: 'D'})
         else if (v.family === 'x' && v.amount === 2) mapping = mergeMappings(mapping, {U: 'D', D: 'U', F: 'B', B: 'F'})
-        else if (v.family === 'z' && v.amount === -1) mapping = mergeMappings(mapping, {U: 'L', D: 'R', R: 'F', L: 'B'})
-        else if (v.family === 'z' && v.amount === 1) mapping = mergeMappings(mapping, {U: 'R', D: 'L', R: 'B', L: 'F'})
+        else if (v.family === 'z' && v.amount === 1) mapping = mergeMappings(mapping, {U: 'L', D: 'R', R: 'U', L: 'D'})
+        else if (v.family === 'z' && v.amount === -1) mapping = mergeMappings(mapping, {U: 'R', D: 'L', R: 'D', L: 'U'})
         else if (v.family === 'z' && v.amount === 2) mapping = mergeMappings(mapping, {U: 'D', D: 'U', R: 'L', L: 'R'})
       }
       parsed.reverse() // process sequence in reverse because future rotations impact the current moves to be performed.
@@ -206,11 +207,11 @@ export default {
         if (rotation) {
           mapRotation(rotation)
         }
-        // TODO: do we still need mapping + invertMapping? (can we just revere all the mapping moves and use invert instead?)
-        processed.push({move: v, rotation, inverseMapping: invertMapping(mapping)})
+        processed.push({move: v, rotation, mapping})
       })
       
       processed.reverse()
+      console.log(processed)
       return processed
     },
     // TODO:
--- src/views/AlgTrainer.vue
+++ src/views/AlgTrainer.vue
@@ -2,7 +2,7 @@
   <div class="algtrainer">
     <button @click='connect'>Connect</button>
     <button @click='reset'>Reset</button>
-    <scramble :scramble='item.alg' :move='move' @execMoves='executeMoves' ref='scramble' />
+    <scramble :scramble='item.alg' :moves='moves' @execMoves='executeMoves' ref='scramble' />
     <div id='twisty'></div>
     <div>{{elapsedTime}}</div>
     <div>{{item}}</div>
@@ -30,19 +30,22 @@ async function asyncSetup(twistyPlayer) {
   })
 }
 
+// eslint-disable-next-line
 const COLL_LIST = [{ name: "B1", alg: "R' U' R U' R' U2 R" }, { name: "B2", alg: "y R' U' R U' R' U R' D' R U R' D R2" }, { name: "B3", alg: "L R' U' R U L' U2 R' U2 R" }, { name: "B4", alg: "R' U' R U R' F R U R' U' R' F' R2" }, { name: "B5", alg: "R' U L U' R U L'" }, { name: "B6", alg: "R U' R' U2 R U' R' U2 R' D' R U R' D R" }, { name: "C1", alg: "R U R' U R U2 R'" }, { name: "C2", alg: "L' U2 L U2 R U' L' U L R'" }, { name: "C3", alg: "L' R U R' U' L U2 R U2 R'" }, { name: "C4", alg: "y' R U R' U R U' R D R' U' R D' R2" }, { name: "C5", alg: "R U' L' U R' U' L" }, { name: "C6", alg: "F' R U2 R' U2 R' F2 R U R U' R' F'" }, { name: "D1", alg: "y' R U2 R' U' R U R' U' R U R' U' R U' R'" }, { name: "D2", alg: "R' U2 R' D' R U2 R' D R2" }, { name: "D3", alg: "y R U2 R D R' U2 R D' R2" }, { name: "D4", alg: "x' R U' R' D R U R' D' x" }, { name: "D5", alg: "y2 F' r U R' U' r' F R" }, { name: "D6", alg: "y' R' U' R U R' F' R U R' U' R' F R2" }, { name: "E1", alg: "R' U' R U' R' U2 R2 U R' U R U2 R'" }, { name: "E2", alg: "R' F R U' R' U' R U R' F' R U R' U' R' F R F' R" }, { name: "E3", alg: "y2 R2 D R' U2 R D' R' U2 R'" }, { name: "E4", alg: "F R U' R' U R U R' U R U' R' F'" }, { name: "E5", alg: "R2 D' R U2 R' D R U2 R" }, { name: "E6", alg: "R' U2 R F U' R' U' R U F'" }, { name: "F1", alg: "R U2 R' U' R U' R2 U2 R U R' U R" }, { name: "F2", alg: "R' U R U2 R' L' U R U' L" }, { name: "F3", alg: "y l' U' L U R U' r' F" }, { name: "F4", alg: "y2 F R U R' U' R U' R' U' R U R' F'" }, { name: "F5", alg: "y' r U R' U' r' F R F'" }, { name: "F6", alg: "R' U R2 D r' U2 r D' R2 U' R" }, { name: "G1", alg: "R U2 R2 U' R2 U' R2 U2 R" }, { name: "G2", alg: "R' F2 R U2 R U2 R' F2 U' R U' R'" }, { name: "G3", alg: "R' U' F' R U R' U' R' F R2 U2 R' U2 R" }, { name: "G4", alg: "R U R' U' R' F R2 U R' U' R U R' U' F'" }, { name: "G5", alg: "R U' L' U R' U L U L' U L" }, { name: "G6", alg: "R U D' R U R' D R2 U' R' U' R2 U2 R" }, { name: "H1", alg: "R U R' U R U' R' U R U2 R'" }, { name: "H2", alg: "F R U' R' U R U2 R' U' R U R' U' F'" }, { name: "H3", alg: "R U R' U R U L' U R' U' L" }, { name: "H4", alg: "y F R U R' U' R U R' U' R U R' U' F'" }]
+const TEST_LIST = [{name: "AA", alg: "y x R2 r L x z L' r2 U F B l' x'"}, {name: "test", alg: "R y x"}, {name: "test", alg: "x y R"}, {name: "test", alg: "x y z R"},  {name: "test", alg: "y' R y'"}, {name: "test", alg: "x' R x'"}, {name: "test", alg: "z' R z'"},  {name: "test", alg: "y x R x"}, {name: "test", alg: "x R"}, {name: "test", alg: "z R"}]
 
 class RandomSelector {
   constructor(length) {
     this.length = length
   }
   select () {
-    return 24
+    return 0
     // return Math.floor(Math.random() * Math.floor(this.length))
   }
 }
 
-class SequentialSelector { // eslint-disable-line
+// eslint-disable-next-line
+class SequentialSelector { 
   constructor(length) {
     this.length = length
     this.start = Math.floor(Math.random() * Math.floor(this.length))
@@ -63,7 +66,7 @@ export default {
   data: () => ({
     twistyPlayer: null, // TODO: display just the cube and not the entire player window
     item: {name: "invalid", alg: ""},
-    move: null,
+    moves: [],
     puzzleState: null,
     selector: null,
 
@@ -80,10 +83,11 @@ export default {
           this.startTimer()
         }
         console.log('received: ', e.latestMove)
-        this.move = { move: e.latestMove, key: Math.random() } // we need to set up a random key so that the watcher in Scramble is updated (eg. U2 = U + U. we need a different key to ensure the moves are separated)
+        this.moves.push(e.latestMove)
         // TODO: ensure that this function isn't too expensive so timing doesn't have latency.
+        // TODO: fix below lines to trigger in executeMoves
         if (EquivalentTransformations(Puzzles['3x3x3'], this.puzzleState.state, new KPuzzle(Puzzles['3x3x3']).state)) {
-          this.stopTimer()
+          this.stopTimer() // TODO: ensure the timer is "stopped" at the moment the last move is made (keep track of that time) instead of the time that processing/computation is finished
           // TODO: add feature to remove this pause.
           setTimeout(() => {
             this.selectNewAlg()
@@ -128,10 +132,10 @@ export default {
     selectNewAlg (i) {
       if (i === undefined) {
         let index = this.selector.select()
-        this.item = COLL_LIST[index]
+        this.item = TEST_LIST[index]
         this.item.index = index
       }
-      this.move = null
+      this.moves = []
       this.$refs.scramble.reset()
       let inverseAlg = invert(parse(this.item.alg))
       this.executeMoves(inverseAlg.nestedUnits)
@@ -142,12 +146,12 @@ export default {
     document.addEventListener('keydown', (e) => {
       if ('urfdlbURFDLB'.split('').includes(e.key)) {
         let m = BareBlockMove(e.key.toUpperCase(), 1)
-        this.move = { move: m, key: Math.random() }
+        this.moves.push(m)
         this.puzzleState.applyBlockMove(m)
       }
     })
     
-    this.selector = new RandomSelector(COLL_LIST.length)
+    this.selector = new RandomSelector(TEST_LIST.length)
     this.reset()
   },
 }
